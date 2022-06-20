9aac23b9e131dd7c41526232b08eaf4c47b0e14d
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-06-20 15:00:29 -0600

    error handling: added error handling to my api request
    - using the catch block and status code I stored any errors in state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [commits, setCommits] = useState([]);
+  const [errors, setErrors] = useState([]);
   const getCommitsRecursively = (commits, index = 0) => {
     if (commits[index + 1]) {
       getCommitsRecursively(commits, index + 1);
@@ -17,13 +18,25 @@ function App() {
     axios({
       method: "get",
       url: "https://api.github.com/repos/gt1990/crossroads-group/commits",
-    }).then((response) => {
-      // console.log("STATUS: ", response.status, response.statusText);
-      console.log(response.data[0].commit);
-      if (response.status === 200) {
-        getCommitsRecursively(response.data);
-      }
-    });
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          getCommitsRecursively(response.data);
+        } else {
+          setErrors((prevState) => {
+            return [...prevState, `${response.status}: ${response.statusText}`];
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrors((prevState) => {
+          return [
+            ...prevState,
+            `${error.response.status}:  ${error.message}. Please try again.`,
+          ];
+        });
+      });
   }, []);
 
   return <h1></h1>;
