0110471384cd875afb766849d03927e364b3cf82
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-06-20 14:57:25 -0600

    feature: github rest api get request
    - using axios I made a get request to the github api in the useEffect hook
    
    - I then stored the commits from the response data in state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function App() {
-  return <h1>Crossroads-Group Assesment</h1>;
+  const [commits, setCommits] = useState([]);
+  const getCommitsRecursively = (commits, index = 0) => {
+    if (commits[index + 1]) {
+      getCommitsRecursively(commits, index + 1);
+    }
+    if (commits[index]) {
+      setCommits((prevState) => {
+        return [...prevState, commits[index].commit];
+      });
+    }
+  };
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "https://api.github.com/repos/gt1990/crossroads-group/commits",
+    }).then((response) => {
+      // console.log("STATUS: ", response.status, response.statusText);
+      console.log(response.data[0].commit);
+      if (response.status === 200) {
+        getCommitsRecursively(response.data);
+      }
+    });
+  }, []);
+
+  return <h1></h1>;
 }
 
 export default App;
