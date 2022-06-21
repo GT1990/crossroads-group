c4aab97dd9b8afdb074282743205c1a0c3425973
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-06-21 01:01:12 -0600

    feature: created commit card
    - added commit message
    
    - added author github username and avatar to card
    
    - added commit timestamp

--- src/components/Commit.js
+++ src/components/Commit.js
@@ -1,5 +1,28 @@
-const Commit = () => {
-  return <div>Commit</div>;
+import "../css/commit.css";
+
+const Commit = ({ data, commitNumber }) => {
+  const date = new Date(data.commit.author.date);
+  const timeStamp = date.toLocaleString("en-US", {
+    timeZone: "America/Los_Angeles",
+    timeZoneName: "short",
+  });
+  console.log("Date: ", date, timeStamp);
+  return (
+    <div className="card">
+      {data.commit.message}
+      <div className="details">
+        {console.log(data)}
+        <img
+          className="avatar"
+          src={data.author.avatar_url}
+          alt="Author's Avatar"
+        />
+        {data.author.login}
+        {}
+      </div>
+      <span className="commit_number">#{commitNumber}</span>
+    </div>
+  );
 };
 
 export default Commit;
